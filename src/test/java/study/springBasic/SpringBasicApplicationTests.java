9c38a48fee295456192c7fe5f8e49cf1c3fef434
최영찬 <gks54410@gmail.com>
2021-11-28 17:36:05 +0900

    Initial commit

---
+++ src/test/java/study/springBasic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springBasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
